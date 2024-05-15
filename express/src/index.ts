682da8be2d280d1b5ae5808b035eb1c4253cd1c9
ricasbp <ricardobaltazarpedro@hotmail.com>
2024-05-15 17:49:53 +0100

    feat: OSC working. VRLists Mock Working. Developing Sse Server

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,28 +1,66 @@
 // src/index.ts
 import express from 'express';
 import cors from 'cors';
-import { Client, Message, Server } from 'node-osc';
-
+import { Client, Message, Server as OSCServer } from 'node-osc';
+import { Server } from 'http';
+import SSE from "sse";
 
 const app = express()
+const server = new Server(app)
+const sse = new SSE(server);
 app.use(cors())
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hi, i´m up');
+    res.send('Hi, im up');
 });
 
 // http://localhost:3000/DownloadAssets
 // JSON body has VR1_IP
-
-app.get('/miau', (req, res) => {
-    const client = new Client('192.168.1.248', 8001);
+app.get('/VRHeadsets', (req, res) => {
+    const client = new Client('10.101.0.16', 8001);
     client.send( new Message("/miau") , () => {
         client.close();
     });
     res.send('Hello, the message was sent');
 });
+
+// Starts sse connection to client
+sse.on('connection', (client) => {
+  const sendEvent = () => {
+      client.send(new Date().toISOString());
+  };
+
+  const intervalId = setInterval(sendEvent, 1000);
+
+  client.on('close', () => {
+      clearInterval(intervalId);
+  });
+});
+
+// Sends Example Message to Client
+app.get('/events', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // flush the headers to establish SSE with client
+
+  const sendEvent = () => {
+      res.write(`data: ${new Date().toISOString()}\n\n`);
+  };
+
+  const intervalId = setInterval(sendEvent, 1000);
+
+  req.on('close', () => {
+      clearInterval(intervalId);
+      res.end();
+  });
+});
+
+//server.listen(port, () => {
+//  console.log(`SSE server running at http://localhost:${port}`);
+//});
 
 // Start the Express server
 app.listen(port, () => {
@@ -30,7 +68,7 @@ app.listen(port, () => {
 });
 
 // Setup OSC server to listen for incoming messages
-const oscServer = new Server(3333, '192.168.1.248', () => {
+const oscServer = new OSCServer(3333, '10.101.0.16', () => {
   console.log('OSC Server is listening on port 3333');
 });
 // Handle incoming OSC messages
@@ -39,5 +77,3 @@ oscServer.on('message', (msg) => {
     // Process the message as needed
     // oscServer.close();
 });
-
-
