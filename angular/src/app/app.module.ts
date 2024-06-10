2417c4117cf368395add9c3edb7fdef985fa8f06
ricasbp <ricardobaltazarpedro@hotmail.com>
2024-06-10 16:39:07 +0100

    Added Ipad and Android PWA

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
 import { VrCustomComponent } from './components/vr-custom/vr-custom.component';
 import { VRInfoComponent } from './components/vr-info/vr-info.component';
 import { ToggleExperienceComponent } from './components/toggle-experience/toggle-experience.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,13 @@ import { ToggleExperienceComponent } from './components/toggle-experience/toggle
     AppRoutingModule,
     FormsModule,
     AsyncPipe,
-    JsonPipe
+    JsonPipe,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
