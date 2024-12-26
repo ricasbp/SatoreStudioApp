27b21577714666da78c63935ce94a2133d6679e4
ricasbp <ricardobaltazarpedro@hotmail.com>
2024-12-26 11:34:56 +0000

    Fix: now SatoreApp.exe installs Node.js on Angular, Express and Node_Modues

--- configurations/TransmiXR.bat
+++ configurations/TransmiXR.bat
@@ -12,19 +12,16 @@ REM Navigate to the Angular project directory
 cd /d "%USERPROFILE%\Desktop\SatoreStudioApp-main\angular"
 
 REM Start the Angular application in a minimized command prompt window with a custom title
-start /min cmd /k "title Angular Front-End & ng serve"
+start /min cmd /k "title Angular Front-End & npm install & ng serve"
 
 REM Navigate to the Express server directory
 cd /d "%USERPROFILE%\Desktop\SatoreStudioApp-main\express"
 
 REM Start the Express server in a minimized command prompt window with a custom title
-start /min cmd /k "title Express Back-End & npm start"
+start /min cmd /k "title Express Back-End & npm install & npm start"
 
 REM Navigate to the Angular project directory
 cd /d "%USERPROFILE%\Desktop\SatoreStudioApp-main"
 
 REM Run ngrok without minimizing the terminal and with a custom title
 start cmd /k "title Ngrok & ngrok start --all"
-
-REM Pause the terminal after execution
-pause
